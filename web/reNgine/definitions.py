45a354b785051e8af1108e1fd0c4e1b24767d831
thinhz2k1 <thinhz2k1@gmail.com>
2024-03-03 22:18:17 +0700

    up rengine

---
+++ web/reNgine/definitions.py
@@ -0,0 +1,483 @@
+#!/usr/bin/python
+import logging
+import re
+
+###############################################################################
+# TOOLS DEFINITIONS
+###############################################################################
+logger = logging.getLogger('django')
+
+###############################################################################
+# TOOLS DEFINITIONS
+###############################################################################
+
+EMAIL_REGEX = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
+
+###############################################################################
+# YAML CONFIG DEFINITIONS
+###############################################################################
+
+ALL = 'all'
+AMASS_WORDLIST = 'amass_wordlist'
+AUTO_CALIBRATION = 'auto_calibration'
+CUSTOM_HEADER = 'custom_header'
+FETCH_GPT_REPORT = 'fetch_gpt_report'
+RUN_NUCLEI = 'run_nuclei'
+RUN_CRLFUZZ = 'run_crlfuzz'
+RUN_DALFOX = 'run_dalfox'
+RUN_S3SCANNER = 'run_s3scanner'
+DIR_FILE_FUZZ = 'dir_file_fuzz'
+FOLLOW_REDIRECT = 'follow_redirect'
+EXTENSIONS = 'extensions'
+EXCLUDED_SUBDOMAINS = 'exclude_subdomains'
+EXCLUDE_EXTENSIONS = 'exclude_extensions'
+EXCLUDE_TEXT = 'exclude_text'
+FETCH_URL = 'fetch_url'
+GF_PATTERNS = 'gf_patterns'
+HTTP_CRAWL = 'http_crawl'
+IGNORE_FILE_EXTENSION = 'ignore_file_extensions'
+INTENSITY = 'intensity'
+MATCH_HTTP_STATUS = 'match_http_status'
+MAX_TIME = 'max_time'
+NAABU_EXCLUDE_PORTS = 'exclude_ports'
+NAABU_EXCLUDE_SUBDOMAINS = 'exclude_subdomains'
+ENABLE_NMAP = 'enable_nmap'
+NMAP_COMMAND = 'nmap_cmd'
+NMAP_SCRIPT = 'nmap_script'
+NMAP_SCRIPT_ARGS = 'nmap_script_args'
+NAABU_PASSIVE = 'passive'
+NAABU_RATE = 'rate'
+NUCLEI_CUSTOM_TEMPLATE = 'custom_templates'
+NUCLEI_TAGS = 'tags'
+NUCLEI_TEMPLATE = 'templates'
+NUCLEI_SEVERITY = 'severities'
+NUCLEI_CONCURRENCY = 'concurrency'
+OSINT = 'osint'
+OSINT_DOCUMENTS_LIMIT = 'documents_limit'
+OSINT_DISCOVER = 'discover'
+OSINT_DORK = 'dorks'
+OSINT_CUSTOM_DORK = 'custom_dorks'
+PORT = 'port'
+PORTS = 'ports'
+RECURSIVE = 'recursive'
+RECURSIVE_LEVEL = 'recursive_level'
+PORT_SCAN = 'port_scan'
+RATE_LIMIT = 'rate_limit'
+RETRIES = 'retries'
+SCREENSHOT = 'screenshot'
+SUBDOMAIN_DISCOVERY = 'subdomain_discovery'
+STOP_ON_ERROR = 'stop_on_error'
+ENABLE_HTTP_CRAWL = 'enable_http_crawl'
+THREADS = 'threads'
+TIMEOUT = 'timeout'
+USE_AMASS_CONFIG = 'use_amass_config'
+USE_NAABU_CONFIG = 'use_naabu_config'
+USE_NUCLEI_CONFIG = 'use_nuclei_config'
+USE_SUBFINDER_CONFIG = 'use_subfinder_config'
+USES_TOOLS = 'uses_tools'
+VULNERABILITY_SCAN = 'vulnerability_scan'
+WAF_DETECTION = 'waf_detection'
+WORDLIST = 'wordlist_name'
+REMOVE_DUPLICATE_ENDPOINTS = 'remove_duplicate_endpoints'
+DUPLICATE_REMOVAL_FIELDS = 'duplicate_fields'
+DALFOX = 'dalfox'
+S3SCANNER = 's3scanner'
+NUCLEI = 'nuclei'
+NMAP = 'nmap'
+CRLFUZZ = 'crlfuzz'
+WAF_EVASION = 'waf_evasion'
+BLIND_XSS_SERVER = 'blind_xss_server'
+USER_AGENT = 'user_agent'
+DELAY = 'delay'
+PROVIDERS = 'providers'
+
+###############################################################################
+# Scan DEFAULTS
+###############################################################################
+
+LIVE_SCAN = 1
+SCHEDULED_SCAN = 0
+
+DEFAULT_SCAN_INTENSITY = 'normal'
+
+###############################################################################
+# Tools DEFAULTS
+###############################################################################
+
+# amass
+AMASS_DEFAULT_WORDLIST_PATH = (
+    'wordlist/default_wordlist/deepmagic.com-prefixes-top50000.txt'
+)
+
+# dorks
+DORKS_DEFAULT_NAMES = [
+    'stackoverflow',
+    '3rdparty',
+    'social_media',
+    'project_management',
+    'code_sharing',
+    'config_files',
+    'jenkins',
+    'cloud_buckets',
+    'php_error',
+    'exposed_documents',
+    'struts_rce',
+    'db_files',
+    'traefik',
+    'git_exposed'
+]
+
+# ffuf
+FFUF_DEFAULT_WORDLIST_PATH = '/usr/src/wordlist/dicc.txt'
+FFUF_DEFAULT_MATCH_HTTP_STATUS = [200, 204]
+FFUF_DEFAULT_RECURSIVE_LEVEL = 2
+FFUF_DEFAULT_FOLLOW_REDIRECT = False
+
+# naabu
+NAABU_DEFAULT_PORTS = ['top-100']
+
+# nuclei
+NUCLEI_DEFAULT_TEMPLATES_PATH = '/root/nuclei-templates'
+NUCLEI_SEVERITY_MAP = {
+    'info': 0,
+    'low': 1,
+    'medium': 2,
+    'high': 3,
+    'critical': 4,
+    'unknown': -1,
+}
+NUCLEI_REVERSE_SEVERITY_MAP = {v: k for k, v in NUCLEI_SEVERITY_MAP.items()}
+NUCLEI_DEFAULT_SEVERITIES = list(NUCLEI_SEVERITY_MAP.keys())
+
+# s3scanner
+S3SCANNER_DEFAULT_PROVIDERS = ['gcp', 'aws', 'digitalocean', 'dreamhost', 'linode']
+
+# dalfox
+DALFOX_SEVERITY_MAP = {
+    'Low': 1,
+    'Medium': 2,
+    'High': 3,
+    'unknown': -1,
+}
+
+# osint
+OSINT_DEFAULT_LOOKUPS = ['emails', 'metainfo', 'employees']
+OSINT_DEFAULT_DORKS = [
+    'stackoverflow',
+    '3rdparty',
+    'social_media',
+    'project_management',
+    'code_sharing',
+    'config_files',
+    'jenkins',
+    'wordpress_files',
+    'cloud_buckets',
+    'php_error',
+    'exposed_documents',
+    'struts_rce',
+    'db_files',
+    'traefik',
+    'git_exposed',
+]
+OSINT_DEFAULT_CONFIG = {
+    'discover': OSINT_DEFAULT_LOOKUPS,
+    'dork': OSINT_DEFAULT_DORKS
+}
+
+# subdomain scan
+SUBDOMAIN_SCAN_DEFAULT_TOOLS = ['subfinder', 'ctfr', 'sublist3r', 'tlsx']
+
+# endpoints scan
+ENDPOINT_SCAN_DEFAULT_TOOLS = ['gospider']
+ENDPOINT_SCAN_DEFAULT_DUPLICATE_FIELDS = ['content_length', 'page_title']
+
+
+###############################################################################
+# Logger DEFINITIONS
+###############################################################################
+
+CONFIG_FILE_NOT_FOUND = 'Config file not found'
+
+###############################################################################
+# Preferences DEFINITIONS
+###############################################################################
+
+SMALL = '100px'
+MEDIM = '200px'
+LARGE = '400px'
+XLARGE = '500px'
+
+# Discord message colors
+DISCORD_INFO_COLOR = '0xfbbc00' # yellow
+DISCORD_WARNING_COLOR = '0xf75b00' # orange
+DISCORD_ERROR_COLOR = '0xf70000'
+DISCORD_SUCCESS_COLOR = '0x00ff78'
+DISCORD_SEVERITY_COLORS = {
+    'info': DISCORD_INFO_COLOR,
+    'warning': DISCORD_WARNING_COLOR,
+    'error': DISCORD_ERROR_COLOR,
+    'aborted': DISCORD_ERROR_COLOR,
+    'success': DISCORD_SUCCESS_COLOR
+}
+
+STATUS_TO_SEVERITIES = {
+    'RUNNING': 'info',
+    'SUCCESS': 'success',
+    'FAILED': 'error',
+    'ABORTED': 'error'
+}
+
+###############################################################################
+# Interesting Subdomain DEFINITIONS
+###############################################################################
+MATCHED_SUBDOMAIN = 'Subdomain'
+MATCHED_PAGE_TITLE = 'Page Title'
+
+###############################################################################
+# Celery Task Status CODES
+###############################################################################
+INITIATED_TASK = -1
+FAILED_TASK = 0
+RUNNING_TASK = 1
+SUCCESS_TASK = 2
+ABORTED_TASK = 3
+
+CELERY_TASK_STATUS_MAP = {
+    INITIATED_TASK: 'INITITATED',
+    FAILED_TASK: 'FAILED',
+    RUNNING_TASK: 'RUNNING',
+    SUCCESS_TASK: 'SUCCESS',
+    ABORTED_TASK: 'ABORTED'
+}
+
+CELERY_TASK_STATUSES = (
+    (INITIATED_TASK, INITIATED_TASK),
+    (FAILED_TASK, FAILED_TASK),
+    (RUNNING_TASK, RUNNING_TASK),
+    (SUCCESS_TASK, SUCCESS_TASK),
+    (ABORTED_TASK, ABORTED_TASK)
+)
+DYNAMIC_ID = -1
+
+###############################################################################
+# Uncommon Ports
+# Source: https://github.com/six2dez/reconftw/blob/main/reconftw.cfg
+###############################################################################
+UNCOMMON_WEB_PORTS = [
+    81,
+    300,
+    591,
+    593,
+    832,
+    981,
+    1010,
+    1311,
+    1099,
+    2082,
+    2095,
+    2096,
+    2480,
+    3000,
+    3128,
+    3333,
+    4243,
+    4567,
+    4711,
+    4712,
+    4993,
+    5000,
+    5104,
+    5108,
+    5280,
+    5281,
+    5601,
+    5800,
+    6543,
+    7000,
+    7001,
+    7396,
+    7474,
+    8000,
+    8001,
+    8008,
+    8014,
+    8042,
+    8060,
+    8069,
+    8080,
+    8081,
+    8083,
+    8088,
+    8090,
+    8091,
+    8095,
+    8118,
+    8123,
+    8172,
+    8181,
+    8222,
+    8243,
+    8280,
+    8281,
+    8333,
+    8337,
+    8443,
+    8500,
+    8834,
+    8880,
+    8888,
+    8983,
+    9000,
+    9001,
+    9043,
+    9060,
+    9080,
+    9090,
+    9091,
+    9200,
+    9443,
+    9502,
+    9800,
+    9981,
+    10000,
+    10250,
+    11371,
+    12443,
+    15672,
+    16080,
+    17778,
+    18091,
+    18092,
+    20720,
+    32000,
+    55440,
+    55672,
+]
+
+###############################################################################
+# WHOIS DEFINITIONS
+# IGNORE_WHOIS_RELATED_KEYWORD: To ignore and disable finding generic related domains
+###############################################################################
+
+IGNORE_WHOIS_RELATED_KEYWORD = [
+    'Registration Private',
+    'Domains By Proxy Llc',
+    'Redacted For Privacy',
+    'Digital Privacy Corporation',
+    'Private Registrant',
+    'Domain Administrator',
+    'Administrator',
+]
+
+
+# Default FETCH URL params
+DEFAULT_IGNORE_FILE_EXTENSIONS = [
+    'png',
+    'jpg',
+    'jpeg',
+    'gif',
+    'mp4',
+    'mpeg',
+    'mp3',
+]
+
+DEFAULT_GF_PATTERNS = [
+    'debug_logic',
+    'idor',
+    'interestingEXT',
+    'interestingparams',
+    'interestingsubs',
+    'lfi',
+    'rce',
+    'redirect',
+    'sqli',
+    'ssrf',
+    'ssti',
+    'xss'
+]
+
+
+# Default Dir File Fuzz Params
+DEFAULT_DIR_FILE_FUZZ_EXTENSIONS =  [
+    '.html',
+    '.php',
+    '.git',
+    '.yaml',
+    '.conf',
+    '.cnf',
+    '.config',
+    '.gz',
+    '.env',
+    '.log',
+    '.db',
+    '.mysql',
+    '.bak',
+    '.asp',
+    '.aspx',
+    '.txt',
+    '.conf',
+    '.sql',
+    '.json',
+    '.yml',
+    '.pdf',
+]
+
+# Roles and Permissions
+PERM_MODIFY_SYSTEM_CONFIGURATIONS = 'modify_system_configurations'
+PERM_MODIFY_SCAN_CONFIGURATIONS = 'modify_scan_configurations'
+PERM_MODIFY_TARGETS = 'modify_targets'
+PERM_MODIFY_SCAN_RESULTS = 'modify_scan_results'
+PERM_MODIFY_WORDLISTS = 'modify_wordlists'
+PERM_MODIFY_INTERESTING_LOOKUP = 'modify_interesting_lookup'
+PERM_MODIFY_SCAN_REPORT = 'modify_scan_report'
+PERM_INITATE_SCANS_SUBSCANS = 'initiate_scans_subscans'
+
+# 404 page url
+FOUR_OH_FOUR_URL = '/404/'
+
+
+# GPT Vulnerability Report Generator
+VULNERABILITY_DESCRIPTION_SYSTEM_MESSAGE = """
+    You are a highly skilled penetration tester who has recently completed a penetration testing.
+    You will be given with a
+        - Vulnerability title
+        - Vulnerable URL
+        - and some description about the vulnerability.
+    Your job is to write a detailed technical penetration testing report based on the given Vulnerability details.
+    The purpose of this report is to provide an in-depth analysis of the vulnerabilities discovered during the penetration testing engagement.
+
+    The penetration testing report must contain all separated by \n\n
+
+    - Vulnerability description
+        Include a detailed vulnerability description, include any known CVE IDs, any known existing vulnerabilities.
+    - Impact
+        Include what this vulnerability can impact for web applications.
+    - Remediation
+        Include steps to remediate this vulnerability. Separate each new remediations by - and a new line \n
+    - References
+        Include any references URL about this vulnerability, any existing CVE ID, or news articles etc. Separate each new references by - and a new line \n. Only include http urls
+
+    Do not write 'Penetration Testing Report:' on the title.
+"""
+
+
+ATTACK_SUGGESTION_GPT_SYSTEM_PROMPT = """
+    You are a highly skilled penetration tester who has recently completed a reconnaissance on a target.
+    As a penetration tester, I've conducted a thorough reconnaissance on a specific subdomain.
+    Based on my reconnaissance you will be given with a
+        - Subdomain Name
+        - Subdomain Page Title
+        - Open Ports if any detected
+        - HTTP Status
+        - Technologies Detected
+        - Content Type
+        - Web Server
+        - Page Content Length
+    I'm seeking insights into potential technical web application attacks that could be executed on this subdomain, along with explanations for why these attacks are feasible given the discovered information.
+    Please provide a detailed list of these attack types and their underlying technical rationales on every attacks you suggested.
+    Also suggest if any CVE ID, known exploits, existing vulnerabilities, any news articles URL related to the information provided to you.
+"""
+
+
+# OSINT GooFuzz Path
+GOFUZZ_EXEC_PATH = '/usr/src/github/goofuzz/GooFuzz'
---
+++ web/reNgine/settings.py
@@ -0,0 +1,278 @@
+import mimetypes
+import os
+
+from reNgine.init import first_run
+from reNgine.utilities import RengineTaskFormatter
+
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#       RENGINE CONFIGURATIONS
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# Root env vars
+RENGINE_HOME = os.environ.get('RENGINE_HOME', '/usr/src/app')
+RENGINE_RESULTS = os.environ.get('RENGINE_RESULTS', f'{RENGINE_HOME}/scan_results')
+RENGINE_CACHE_ENABLED = bool(int(os.environ.get('RENGINE_CACHE_ENABLED', '0')))
+RENGINE_RECORD_ENABLED = bool(int(os.environ.get('RENGINE_RECORD_ENABLED', '1')))
+RENGINE_RAISE_ON_ERROR = bool(int(os.environ.get('RENGINE_RAISE_ON_ERROR', '0')))
+
+# Common env vars
+DEBUG = bool(int(os.environ.get('DEBUG', '0')))
+DOMAIN_NAME = os.environ.get('DOMAIN_NAME', 'localhost:8000')
+TEMPLATE_DEBUG = bool(int(os.environ.get('TEMPLATE_DEBUG', '0')))
+SECRET_FILE = os.path.join(RENGINE_HOME, 'secret')
+DEFAULT_ENABLE_HTTP_CRAWL = bool(int(os.environ.get('DEFAULT_ENABLE_HTTP_CRAWL', '1')))
+DEFAULT_RATE_LIMIT = int(os.environ.get('DEFAULT_RATE_LIMIT', '150')) # requests / second
+DEFAULT_HTTP_TIMEOUT = int(os.environ.get('DEFAULT_HTTP_TIMEOUT', '5')) # seconds
+DEFAULT_RETRIES = int(os.environ.get('DEFAULT_RETRIES', '1'))
+DEFAULT_THREADS = int(os.environ.get('DEFAULT_THREADS', '30'))
+DEFAULT_GET_GPT_REPORT = bool(int(os.environ.get('DEFAULT_GET_GPT_REPORT', '1')))
+
+# Globals
+ALLOWED_HOSTS = ['*']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = first_run(SECRET_FILE, BASE_DIR)
+
+# Databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        # 'OPTIONS':{
+        #     'sslmode':'verify-full',
+        #     'sslrootcert': os.path.join(BASE_DIR, 'ca-certificate.crt')
+        # }
+    }
+}
+
+# Application definition
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'rest_framework',
+    'rest_framework_datatables',
+    'dashboard.apps.DashboardConfig',
+    'targetApp.apps.TargetappConfig',
+    'scanEngine.apps.ScanengineConfig',
+    'startScan.apps.StartscanConfig',
+    'recon_note.apps.ReconNoteConfig',
+    'django_ace',
+    'django_celery_beat',
+    'mathfilters',
+    'drf_yasg',
+    'rolepermissions'
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'login_required.middleware.LoginRequiredMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [(os.path.join(BASE_DIR, 'templates'))],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'reNgine.context_processors.projects'
+            ],
+    },
+}]
+ROOT_URLCONF = 'reNgine.urls'
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_datatables.renderers.DatatablesRenderer',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_datatables.filters.DatatablesFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS':(
+        'rest_framework_datatables.pagination.DatatablesPageNumberPagination'
+    ),
+    'PAGE_SIZE': 500,
+}
+WSGI_APPLICATION = 'reNgine.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+                'NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Temporary fix for celery beat crash
+# See https://github.com/yogeshojha/rengine/issues/971
+DJANGO_CELERY_BEAT_TZ_AWARE = False
+
+MEDIA_URL = '/media/'
+FILE_UPLOAD_MAX_MEMORY_SIZE = 100000000
+FILE_UPLOAD_PERMISSIONS = 0o644
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+LOGIN_REQUIRED_IGNORE_VIEW_NAMES = [
+    'login',
+]
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'onboarding'
+LOGOUT_REDIRECT_URL = 'login'
+
+# Tool Location
+TOOL_LOCATION = '/usr/src/app/tools/'
+
+# Number of endpoints that have the same content_length
+DELETE_DUPLICATES_THRESHOLD = 10
+
+'''
+CELERY settings
+'''
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_ENABLE_UTC = False
+CELERY_TIMEZONE = 'UTC'
+CELERY_IGNORE_RESULTS = False
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+CELERY_TRACK_STARTED = True
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+'''
+ROLES and PERMISSIONS
+'''
+ROLEPERMISSIONS_MODULE = 'reNgine.roles'
+ROLEPERMISSIONS_REDIRECT_TO_LOGIN = True
+
+'''
+Cache settings
+'''
+RENGINE_TASK_IGNORE_CACHE_KWARGS = ['ctx']
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+LOGGING settings
+'''
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        'null': {
+            'class': 'logging.NullHandler'
+        },
+        'default': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+        'brief': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'brief'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'brief'
+        },
+        'task': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'task'
+        },
+        'db': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'brief',
+            'filename': 'db.log',
+            'maxBytes': 1024,
+            'backupCount': 3
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(message)s'
+        },
+        'brief': {
+            'format': '%(name)-10s | %(message)s'
+        },
+        'task': {
+            '()': lambda : RengineTaskFormatter('%(task_name)-34s | %(levelname)s | %(message)s')
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['brief'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+            'propagate': False
+        },
+        'celery.app.trace': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'celery.task': {
+            'handlers': ['task'],
+            'propagate': False
+        },
+        'celery.worker': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console'],
+            'propagate': False
+        },
+        'django.db.backends': {
+            'handlers': ['db'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'reNgine.tasks': {
+            'handlers': ['task'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+            'propagate': False
+        }
+    },
+}
